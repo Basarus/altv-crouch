c7a2ab729fe12b05cc0977661212ac98cdeb0489
Basar <74671944+Basarus@users.noreply.github.com>
2021-08-20 16:21:57 +0300

    bug fixe
    This option is clearly better, since it does not require a constant interval

--- altv-crouch/client/client.mjs
+++ altv-crouch/client/client.mjs
@@ -1,47 +1,45 @@
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-natives" />
 import * as alt from 'alt-client';
-import * as native from 'natives'; 
+import * as native from 'natives';
 
 const player = alt.Player.local;
 
-let crouch = false; 
-let hotkey = 87; // Key : W 
+let hotkey = 67; // Key : C
+let interval;
 
-native.requestClipSet("MOVE_M@TOUGH_GUY@");
-native.requestClipSet("move_ped_crouched");
-native.requestClipSet("move_ped_crouched_strafing");
-
-alt.on('keydown', (key) => {
-    if(key === hotkey && !player.vehicle){
-        if(!crouch){
+alt.on('keydown', key => {
+    if (key === hotkey && !player.vehicle) {
+        if (!interval) {
+            native.requestClipSet('MOVE_M@TOUGH_GUY@');
+            native.requestClipSet('move_ped_crouched');
+            native.requestClipSet('move_ped_crouched_strafing');
             native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
-            crouch = true; 
-        }else{
-            native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
-            
-            native.setPedMovementClipset(player.scriptID, "MOVE_M@TOUGH_GUY@", 0.25);
-            alt.setTimeout(() => {
-                native.resetPedMovementClipset(player.scriptID, 0);
-                native.resetPedStrafeClipset(player.scriptID, 0);
-            }, 200);
-            crouch = false; 
+            interval = alt.setInterval(crouch, 500);
         }
     }
 });
 
-alt.setInterval(() => {
-    if(crouch){
-        native.setPedMovementClipset(player.scriptID, "move_ped_crouched", 0.25);
-        native.disableControlAction(0,22,true); //Jump
+alt.on('keyup', key => {
+    if (key === hotkey && interval) {
+        alt.clearInterval(interval);
+        interval = undefined;
         native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
-
-        if(native.isAimCamActive()){
-            native.setPedStrafeClipset(player.scriptID, "move_ped_crouched_strafing");
-            native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
-        }
-    }else{
-        native.resetPedMovementClipset(player.scriptID, 0);
-        native.resetPedStrafeClipset(player.scriptID, 0);
+        native.setPedMovementClipset(player.scriptID, 'MOVE_M@TOUGH_GUY@', 0.25);
+        alt.setTimeout(() => {
+            native.resetPedMovementClipset(player.scriptID, 0);
+            native.resetPedStrafeClipset(player.scriptID, 0);
+        }, 200);
     }
-}, 10)
+});
+
+function crouch() {
+    native.setPedMovementClipset(player.scriptID, 'move_ped_crouched', 0.25);
+    native.disableControlAction(0, 22, true); //Jump
+    native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
+
+    if (native.isAimCamActive()) {
+        native.setPedStrafeClipset(player.scriptID, 'move_ped_crouched_strafing');
+        native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
+    }
+}
